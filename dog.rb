8f64443dee0ad971493529587cdb920a898a30b9
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-08-02 16:32:22 +0000

    create dog class add methods

---
+++ dog.rb
@@ -0,0 +1,15 @@
+require "./animal.rb"
+
+class Dog < Animal
+  def initialize(color, name = "Unknown")
+    super("dog", 4, name)
+    @color = color
+  end
+
+  def bring_a_stick
+    "Here is your stick: ---------"
+  end
+  def speak
+    "Woof, Woof"
+  end
+end
